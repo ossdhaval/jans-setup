7224f1e4ba956a38cd4d37027e46ea33f9dd38e1
HemantKMehta <70174684+HemantKMehta@users.noreply.github.com>
2021-04-05 14:17:16 +0530

    fix: registration.py for cert
    fixed the code for getting cert in script to call getvalue1 method

--- static/extension/client_registration/Registration.py
+++ static/extension/client_registration/Registration.py
@@ -7,7 +7,7 @@ from io.jans.model.custom.script.type.client import ClientRegistrationType
 from io.jans.service.cdi.util import CdiUtil
 from io.jans.as.model.util import JwtUtil
 from io.jans.as.model.util import CertUtils
-from javax.faces.context import FacesContext
+
 
 import java
 
@@ -27,22 +27,15 @@ class ClientRegistration(ClientRegistrationType):
 
     def createClient(self, registerRequest, client, configurationAttributes):
         print "Client registration. CreateClient method"
-        facesContext = CdiUtil.bean(FacesContext)
-        request = facesContext.getExternalContext().getRequest()
-        clientCertAsPem = request.getHeader("X-ClientCert")
-        cert = CertUtils.x509CertificateFromPem(clientCertAsPem)
-        if (clientCertAsPem == None):
-            print "Client registration. cert not found"	
-        else: 
-            cn = CertUtils.getCN(cert)
-            print "Client registration. cn: " + cn
 
-            client.setDn("inum=" + cn + ",ou=clients,o=jans")
-            client.setClientId(cn)
+        cert = CertUtils.x509CertificateFromPem(configurationAttributes.get("certProperty").getValue1())
+        cn = CertUtils.getCN(cert)
+        print "Client registration. cn: " + cn
 
-            return True
+        client.setDn("inum=" + cn + ",ou=clients,o=jans")
+        client.setClientId(cn)
+
         
-        return False
 
     def updateClient(self, registerRequest, client, configurationAttributes):
         print "Client registration. UpdateClient method"
